022a32977e5d2662ae06df8c07b3e9cc445740af
Jeff Campos <jefferson.jarbas@gmail.com>
2021-10-04 08:07:11 -0300

    initial setup

---
+++ src/test/java/com/jeffcampos/cursomc/CursoModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffcampos.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
