c2c9cc8ab9dedcd1189e4a5a1cf63afeb921d9d6
Jeff Campos <jefferson.jarbas@gmail.com>
2021-10-05 08:44:03 -0300

    Testando a primeira classe de dominio Categoria

--- src/main/java/com/jeffcampos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jeffcampos/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.jeffcampos.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jeffcampos.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-		
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Está funfando";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 	}
 }
