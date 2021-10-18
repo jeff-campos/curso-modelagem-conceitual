d55070b3fc93f47d8ef4adac4f864b89d8a2c3c0
Jeff Campos <jefferson.jarbas@gmail.com>
2021-10-18 08:05:38 -0300

    Criando instanciação ao iniciar a aplicação

--- src/main/java/com/jeffcampos/cursomc/CursoModelagemConceitualApplication.java
+++ src/main/java/com/jeffcampos/cursomc/CursoModelagemConceitualApplication.java
@@ -1,13 +1,32 @@
 package com.jeffcampos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jeffcampos.cursomc.domain.Categoria;
+import com.jeffcampos.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoModelagemConceitualApplication {
+public class CursoModelagemConceitualApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritótio");
+		Categoria cat3 = new Categoria(null, "Bazar");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 
 }
