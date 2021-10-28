eaa7cf683e1c9e351f633dd3ffe061d86d34d95d
Jeff Campos <jefferson.jarbas@gmail.com>
2021-10-28 08:19:00 -0300

    Criando Cidade e Estados

--- src/main/java/com/jeffcampos/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/jeffcampos/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -16,6 +16,7 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		// A classe que representa as respostas HTTP, para o status code 404 passará no corpo da sua respota o objeto montado na linha acima.
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 }
